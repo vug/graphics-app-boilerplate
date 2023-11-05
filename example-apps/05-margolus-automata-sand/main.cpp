366efe97feff17a7b146b2ea4cc007bbf6e26f9a
Ugur Guney <ugurguney@gmail.com>
2023-11-05 01:48:58 -0400

    Implement and apply rules on CPU for sand simulation

--- example-apps/05-margolus-automata-sand/main.cpp
+++ example-apps/05-margolus-automata-sand/main.cpp
@@ -21,6 +21,7 @@
 
 #include <iostream>
 #include <print>
+#include <random>
 
 void cudaOnErrorPrintAndExit() {
   cudaError_t error = cudaGetLastError();
@@ -30,9 +31,151 @@ void cudaOnErrorPrintAndExit() {
   }
 }
 
+struct Grid {
+  std::vector<uint32_t> data{};
+  glm::uvec2 size{};
+  glm::uvec2 size2{};
+};
+
+Grid gridCreate(uint32_t nx, uint32_t ny) {
+  Grid g;
+  g.size = {nx, ny}; // simulation area
+  g.size2 = {nx + 2, ny + 2}; // simulation area + boundaries
+  const uint32_t numCells = g.size2.y * g.size2.x;
+  g.data.resize(numCells, 0);
+  return g;
+}
+
+void gridResetBoundaries(Grid& grid) {
+  // Side walls: By trial and error discovered that this "dashed" pattern acts like a wall
+  // ` = 1` results in creation of particles because boundaries are reset after iterations
+  for (int i = 0;  i < grid.size2.y; ++i) {
+    const size_t ixLeft = i * grid.size2.x;
+    const size_t ixRight = ixLeft + grid.size2.x - 1;
+    grid.data[ixLeft] = grid.data[ixRight]  = i % 2;
+  }
+  for (int j = 0; j < grid.size2.x; j++) {
+    const size_t ixTop = j;
+    const size_t ixBottom = (grid.size2.y - 1) * grid.size2.x + j;
+    grid.data[ixTop] = 0; // (0, j)
+    grid.data[ixBottom] = 1; // (ny + 1, j)
+  }
+}
+
+void gridClear(Grid& grid) {
+  const size_t numCells = grid.size2.y * grid.size2.x;
+  for (size_t ix = 0; ix < numCells; ++ix)
+    grid.data[ix] = 0;
+  gridResetBoundaries(grid);
+}
+
+void gridAddRandomCells(Grid& grid, uint32_t numRandomCells) {
+  std::random_device rndDev;
+  std::mt19937 gen(rndDev());
+  std::uniform_int_distribution<> uDistX(1, grid.size2.x - 2);
+  std::uniform_int_distribution<> uDistY(1, grid.size2.y - 2);
+  for (int i = 0; i < numRandomCells; ++i)
+    grid.data[uDistY(gen) * grid.size2.x + uDistX(gen)] = 1;  // (rndX, rndY)
+}
+
+uint32_t gridGetBlockCase(const Grid& grid, uint32_t i, uint32_t j) {
+  // clock-wise from north-west / top-left of 2x2 block
+  const size_t offsetTopLeft = i * grid.size2.x + j; 
+  const size_t offsetTopRight = i * grid.size2.x + (j + 1); 
+  const size_t offsetBottomRight = (i + 1) * grid.size2.x + (j + 1); 
+  const size_t offsetBottomLeft = (i + 1) * grid.size2.x + j;
+  const uint32_t topLeft = grid.data[offsetTopLeft];
+  const uint32_t topRight = grid.data[offsetTopRight];
+  const uint32_t bottomRight = grid.data[offsetBottomRight];
+  const uint32_t bottomLeft = grid.data[offsetBottomLeft];
+  uint32_t result = (topLeft << 3) + (topRight << 2) + (bottomRight << 1) + (bottomLeft << 0);
+  return result;
+}
+
+void gridSetBlockCase(Grid& grid, uint32_t i, uint32_t j, uint32_t blockCase) {
+  const size_t offsetTopLeft = i * grid.size2.x + j;
+  const size_t offsetTopRight = i * grid.size2.x + (j + 1);
+  const size_t offsetBottomRight = (i + 1) * grid.size2.x + (j + 1);
+  const size_t offsetBottomLeft = (i + 1) * grid.size2.x + j;
+  const uint32_t topLeft = (blockCase & 0b1000) == 0b1000;
+  const uint32_t topRight = (blockCase & 0b0100) == 0b0100;
+  const uint32_t bottomRight = (blockCase & 0b0010) == 0b0010;
+  const uint32_t bottomLeft = (blockCase & 0b0001) == 0b0001;
+  //const uint32_t topLeft = (blockCase & 0b0001) == 0b0001;
+  //const uint32_t topRight = (blockCase & 0b0010) == 0b0010;
+  //const uint32_t bottomRight = (blockCase & 0b0100) == 0b0100;
+  //const uint32_t bottomLeft = (blockCase & 0b1000) == 0b1000;
+  grid.data[offsetTopLeft] = topLeft;
+  grid.data[offsetTopRight] = topRight;
+  grid.data[offsetBottomRight] = bottomRight;
+  grid.data[offsetBottomLeft] = bottomLeft;
+}
+
+uint32_t gridApplyRulesToCase(uint32_t oldCase) {
+  switch (oldCase) {
+    case 0b0000:
+      return 0b0000;
+    case 0b0010:
+      return 0b0010;
+    case 0b0001:
+      return 0b0001;
+    case 0b0011:
+      return 0b0011;
+    case 0b0100:
+      return 0b0010;
+    case 0b0110:
+      return 0b0011;
+    case 0b0101:
+      return 0b0011;
+    case 0b0111:
+      return 0b0111;
+    case 0b1000:
+      return 0b0001;
+    case 0b1010:
+      return 0b0011;
+    case 0b1001:
+      return 0b0011;
+    case 0b1011:
+      return 0b1011;
+    case 0b1100:
+      return 0b0011;
+    case 0b1110:
+      return 0b0111;
+    case 0b1101:
+      return 0b1011;
+    case 0b1111:
+      return 0b1111;
+    default:
+      assert(false); // no such case should happen
+  }
+}
+
+void gridApplyRulesToBlock(Grid& grid, uint32_t i, uint32_t j) {
+  uint32_t oldCase = gridGetBlockCase(grid, i, j);
+  uint32_t newCase = gridApplyRulesToCase(oldCase);
+  gridSetBlockCase(grid, i, j, newCase);
+}
+
+void gridApplyRulesToGrid(Grid& grid) {
+  for (int32_t i = grid.size.y - 1; i >= 1; i -= 2) {
+    for (int32_t j = 1; j < grid.size.x; j += 2) {
+      gridApplyRulesToBlock(grid, i, j);
+    }
+  }
+  gridResetBoundaries(grid);
+
+  for (int32_t i = grid.size.y; i >= 0; i -= 2) {
+    for (int32_t j = 0; j < grid.size.x + 1; j += 2) {
+      gridApplyRulesToBlock(grid, i, j);
+    }
+  }
+  gridResetBoundaries(grid);
+}
+
+
 int main(int argc, char* argv[]) {
   std::cout << "Hi!\n";
-  ws::Workshop workshop{800, 600, "Workshop App"};
+  ws::Workshop workshop{1200, 600, "Sand 'Simulation' via Margolus Neighborhood Automata"};
 
   const char *vertexShader = R"(
 #version 460
@@ -74,43 +217,36 @@ void main () {
 }
   )";
   ws::Shader shader{vertexShader, fragmentShader};
-  glm::uvec2 gridSize{10, 10};
-  std::vector<uint32_t> grid(gridSize.x * gridSize.y);
-
+  Grid grid = gridCreate(400, 200); // 60 FPS. (Probably would have been faster if not G-Sync)
+  const int nRnd = 40000;
+  //Grid grid = gridCreate(2400, 1200); // 30 FPS
+  //const int nRnd = 1'440'000;
+  gridResetBoundaries(grid);
+  gridAddRandomCells(grid, nRnd);
   glm::uvec2 winSize = workshop.getWindowSize();
   double aspectRatio = static_cast<double>(winSize.x) / winSize.y;
 
-  uint32_t texHeight = gridSize.x;
+  uint32_t texHeight = grid.size2.y;
   uint32_t texWidth = texHeight * aspectRatio;
   glm::uvec2 offset{0, 0};
-  glm::uvec2 extend{std::min(texWidth, gridSize.x - offset.x), std::min(texHeight, gridSize.y - offset.x)};
+  glm::uvec2 extend{std::min(texWidth, grid.size2.x - offset.x), std::min(texHeight, grid.size2.y - offset.x)};
   std::vector<uint32_t> pixels(extend.x * extend.y);
 
   auto desc = ws::Texture::Specs{extend.x, extend.y, ws::Texture::Format::RGBA8, ws::Texture::Filter::Nearest};
   ws::Texture tex{desc};
 
-  // Calculate the grid
-  for (uint32_t i = 0; i < gridSize.y; ++i) {
-    for (uint32_t j = 0; j < gridSize.x; ++j) {
-      // Red: 0xFF0000FF, Green: 0xFF00FF00, Blue: 0xFFFF0000
-      uint32_t color = 0xFFFFFFFF;
-      if ((i/2 + j/2) % 2 == 0)
-        color = 0xFFFFFF00; // teal
-      else
-        color = 0xFF00FFFF; // yellow
-      grid[i * gridSize.x + j] = color;
-    }
-  }
-
-  // Copy part of it into texture
-  for (uint32_t i = 0; i < extend.y; ++i) {
-    for (uint32_t j = 0; j < extend.x; ++j) {
-      uint32_t gix = (i + offset.y) * gridSize.x + (j + offset.x);
-      uint32_t pix = i * extend.x + j;
-      pixels[pix] = grid[gix];
-    }
-  }
-  tex.loadPixels(pixels.data());
+  //// Calculate the grid
+  //for (uint32_t i = 0; i < gridSize.y; ++i) {
+  //  for (uint32_t j = 0; j < gridSize.x; ++j) {
+  //    // Red: 0xFF0000FF, Green: 0xFF00FF00, Blue: 0xFFFF0000
+  //    uint32_t color = 0xFFFFFFFF;
+  //    if ((i/2 + j/2) % 2 == 0)
+  //      color = 0xFFFFFF00; // teal
+  //    else
+  //      color = 0xFF00FFFF; // yellow
+  //    grid[i * gridSize.x + j] = color;
+  //  }
+  //}
 
   //struct cudaGraphicsResource* texCuda{};
   //cudaGraphicsGLRegisterImage(&texCuda, tex.getId(), GL_TEXTURE_2D, cudaGraphicsRegisterFlagsSurfaceLoadStore);
@@ -134,11 +270,30 @@ void main () {
     static glm::vec3 bgColor{42 / 256.0, 96 / 256.0, 87 / 256.0};
     ImGui::ColorEdit3("BG Color", glm::value_ptr(bgColor));
     ImGui::Separator();
+    if (ImGui::Button("Regenerate")) {
+      gridClear(grid);
+      gridAddRandomCells(grid, nRnd);
+    }
+    ImGui::Separator();
     static bool shouldShowImGuiDemo = false;
     ImGui::Checkbox("Show Demo", &shouldShowImGuiDemo);
     if (shouldShowImGuiDemo)
       ImGui::ShowDemoWindow();
     ImGui::End();
+
+    if (workshop.getFrameNo() % 1 == 0)
+      gridApplyRulesToGrid(grid);
+
+  // Copy part of it into texture
+    for (uint32_t i = 0; i < extend.y; ++i) {
+      for (uint32_t j = 0; j < extend.x; ++j) {
+        //uint32_t gix = (i + offset.y) * grid.size2.x + (j + offset.x);
+        uint32_t gix = ((extend.y - 1 - i) + offset.y) * grid.size2.x + (j + offset.x);
+        uint32_t pix = i * extend.x + j;
+        pixels[pix] = grid.data[gix] * 0xFF00FFFF;
+      }
+    }
+    tex.loadPixels(pixels.data());
 
     //cudaGraphicsMapResources(1, &texCuda, 0);
     //cudaArray_t array;
