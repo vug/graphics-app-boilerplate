e0e217afed08fb7a36477cd81324a98547431559
Ugur Guney <ugurguney@gmail.com>
2023-11-14 20:20:03 -0500

    clean up Mandelbrot stuff

--- example-apps/05-margolus-automata-sand/kernels.h
+++ example-apps/05-margolus-automata-sand/kernels.h
@@ -3,26 +3,9 @@
 #include "cuda_runtime.h"
 #include <curand_kernel.h>
 #include "device_launch_parameters.h"
-#include <glm/vec2.hpp>
-
-enum FractalType : int {
-  Fractal_Mandelbrot = 0,
-  Fractal_Julia = 1,
-};
-
-struct Model {
-  glm::dvec2 topLeft{-0.4f, 0.f};
-  float height = 2.25f;
-  glm::dvec2 z0{0.f, 0.f};
-  int fractalType = Fractal_Mandelbrot; // Mandelbrot or Julia
-  int2 a;
-};
-
-//__global__ void genMandelbrot(cudaSurfaceObject_t surf, int texWidth, int texHeight, Model model, int maxIter, bool useDouble, int timeStep);
-void launchGenMandelbrot(cudaSurfaceObject_t surf, int texWidth, int texHeight, Model model, int maxIter, bool useDouble, int timeStep);
 
 struct GridGPU {
-  uint32_t* cells;
+  unsigned int* cells;
   uint2 size;
   uint2 size2;
 };
