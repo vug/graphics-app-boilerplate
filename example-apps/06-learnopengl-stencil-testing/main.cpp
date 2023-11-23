362d858c89fb24c36d0b84b8a719cafb1734b47e
Ugur Guney <ugurguney@gmail.com>
2023-11-23 12:52:08 -0500

    Skeleton for Stencil Testing

---
+++ example-apps/06-learnopengl-stencil-testing/main.cpp
@@ -0,0 +1,40 @@
+#include <Workshop/Shader.hpp>
+#include <Workshop/Workshop.hpp>
+
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/vec3.hpp>
+
+#include <print>
+
+int main()
+{
+  std::println("Hi!");
+  ws::Workshop workshop{800, 600, "Workshop App"};
+
+  while (!workshop.shouldStop())
+  {
+    workshop.beginFrame();
+
+    ImGui::Begin("Main");
+    static bool shouldShowImGuiDemo = false;
+    ImGui::Checkbox("Show Demo", &shouldShowImGuiDemo);
+    if (shouldShowImGuiDemo)
+      ImGui::ShowDemoWindow();
+
+    static glm::vec3 bgColor{42 / 256.0, 96 / 256.0, 87 / 256.0};
+    ImGui::ColorEdit3("BG Color", glm::value_ptr(bgColor));
+    ImGui::End();
+
+    glClearColor(bgColor.x, bgColor.y, bgColor.z, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    workshop.endFrame();
+  }
+
+  std::println("Bye!");
+  return 0;
+}
