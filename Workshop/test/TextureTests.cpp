eaeed2e07916a532410c3f1cca8259403893f0a5
Ugur Guney <ugurguney@gmail.com>
2023-11-30 22:47:02 -0500

    Test to check texture ID and dimensions

--- Workshop/test/TextureTests.cpp
+++ Workshop/test/TextureTests.cpp
@@ -1,5 +1,20 @@
 #include "WorkshopTest.hpp"
 
+#include <Workshop/Texture.hpp>
+
 TEST_F(WorkshopTest, TextureTest) {
-  EXPECT_EQ(1, 1);
+  ws::Texture::Specs specs {
+    .width = 1024,
+    .height = 768,
+  };
+
+  ws::Texture tex{specs};
+  ASSERT_NE(tex.getId(), ws::INVALID);
+
+  int w, h;
+  const int mipLevel = 0;
+  glGetTextureLevelParameteriv(tex.getId(), mipLevel, GL_TEXTURE_WIDTH, &w);
+  glGetTextureLevelParameteriv(tex.getId(), mipLevel, GL_TEXTURE_HEIGHT, &h);
+  ASSERT_EQ(w, 1024);
+  ASSERT_EQ(h, 768);
 }
