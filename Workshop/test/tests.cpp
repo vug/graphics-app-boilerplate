adde728656205cba83e9da35bf3345858e440733
Ugur Guney <ugurguney@gmail.com>
2023-11-30 20:40:28 -0500

    First two tests: Workshop constructor and begin/end frame won't throw, and glClear works.

--- Workshop/test/tests.cpp
+++ Workshop/test/tests.cpp
@@ -1,9 +1,35 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <Workshop/Workshop.hpp>
+
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+// Make sure that Workshop Constructor and {begin, end}Frame methods won't throw
+TEST(AppTest, NoExceptionsBeginEnd) {
+  // EXPECT_NO_THROW(ws::Workshop ws{800, 600, "Workshop App"}) won't let ws be defined outside of the macro
+  // Therefore unique_ptr is used to be able to access it in later lines
+  std::unique_ptr<ws::Workshop> workshop;
+  EXPECT_NO_THROW(workshop.reset(new ws::Workshop{800, 600, "Workshop App"}));
+  EXPECT_NO_THROW(workshop->beginFrame());
+  EXPECT_NO_THROW(workshop->endFrame());
+}
+
+// Test glClear actually clears the screen with the color given in glClearColor
+TEST(AppTest, ClearColorDepth) {
+  ws::Workshop workshop{800, 600, "Workshop App"};
+  workshop.beginFrame();
+
+  glClearColor(1, 0, 0, 1);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  unsigned char* data = (unsigned char*)malloc(sizeof(unsigned char) * 4);
+  glReadPixels(0, 0, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, data);
+  EXPECT_EQ(data[0], 255);
+  EXPECT_EQ(data[1], 0);
+  EXPECT_EQ(data[2], 0);
+  EXPECT_EQ(data[3], 255);
+
+  workshop.endFrame();
 }
