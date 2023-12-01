31dac93ca716f548433a152ee05035d6266bfd5f
Ugur Guney <ugurguney@gmail.com>
2023-11-30 22:21:52 -0500

    Test fixture class for Workshop
    Adopt ClearColorDepth to fixture.
    New WindowInitialSize, WindowResizedSize (uses glfwSetWindowSize to resize actual window), ViewportInitialSize tests

--- Workshop/test/tests.cpp
+++ Workshop/test/tests.cpp
@@ -7,7 +7,7 @@
 #include <GLFW/glfw3.h>
 
 // Make sure that Workshop Constructor and {begin, end}Frame methods won't throw
-TEST(AppTest, NoExceptionsBeginEnd) {
+TEST(AppSkeletonTest, NoExceptionsBeginEnd) {
   // EXPECT_NO_THROW(ws::Workshop ws{800, 600, "Workshop App"}) won't let ws be defined outside of the macro
   // Therefore unique_ptr is used to be able to access it in later lines
   std::unique_ptr<ws::Workshop> workshop;
@@ -16,11 +16,24 @@ TEST(AppTest, NoExceptionsBeginEnd) {
   EXPECT_NO_THROW(workshop->endFrame());
 }
 
-// Test glClear actually clears the screen with the color given in glClearColor
-TEST(AppTest, ClearColorDepth) {
-  ws::Workshop workshop{800, 600, "Workshop App"};
-  workshop.beginFrame();
+class WorkshopTest : public testing::Test {
+ protected:
+  ws::Workshop workshop;
 
+  WorkshopTest()
+      : workshop{800, 600, "Workshop App"} {}
+
+  void SetUp() override {
+    workshop.beginFrame();
+  }
+
+  void TearDown() override {
+    workshop.endFrame();
+  }
+};
+
+// Test glClear actually clears the screen with the color given in glClearColor
+TEST_F(WorkshopTest, ClearColorDepth) {
   glClearColor(1, 0, 0, 1);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -30,6 +43,43 @@ TEST(AppTest, ClearColorDepth) {
   EXPECT_EQ(data[1], 0);
   EXPECT_EQ(data[2], 0);
   EXPECT_EQ(data[3], 255);
+}
 
-  workshop.endFrame();
+// Test Workshop::getWindowSize returns initial window size
+TEST_F(WorkshopTest, WindowInitialSize) {
+  auto winSize = workshop.getWindowSize();
+  EXPECT_EQ(winSize.x, 800);
+  EXPECT_EQ(winSize.y, 600);
+}
+
+// Test Workshop::getWindowSize returns correct window size after a resize
+TEST_F(WorkshopTest, WindowResizedSize) {
+  glfwSetWindowSize(workshop.getGLFWwindow(), 1024, 768);
+  auto winSize = workshop.getWindowSize();
+  EXPECT_EQ(winSize.x, 1024);
+  EXPECT_EQ(winSize.y, 768);
+}
+
+TEST_F(WorkshopTest, ViewportInitialSize) {
+  GLint m_viewport[4];
+
+  glGetIntegerv(GL_VIEWPORT, m_viewport);
+  EXPECT_EQ(m_viewport[0], 0);
+  EXPECT_EQ(m_viewport[1], 0);
+  EXPECT_EQ(m_viewport[2], 800);
+  EXPECT_EQ(m_viewport[3], 600);
+}
+
+TEST_F(WorkshopTest, ViewportResizedSize) {
+  GLint m_viewport[4];
+
+   glfwSetWindowSize(workshop.getGLFWwindow(), 1024, 768);
+   workshop.endFrame();
+   workshop.beginFrame();
+   glGetIntegerv(GL_VIEWPORT, m_viewport);
+  // TODO: add a glViewport to Workshop::beginFrame
+  // EXPECT_EQ(m_viewport[0], 0);
+  // EXPECT_EQ(m_viewport[1], 0);
+  // EXPECT_EQ(m_viewport[2], 1024);
+  // EXPECT_EQ(m_viewport[3], 768);
 }
