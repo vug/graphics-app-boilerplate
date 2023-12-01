9a17922c2f487241c15e36610ea119304deb17ca
Ugur Guney <ugurguney@gmail.com>
2023-11-30 23:33:25 -0500

    Texture tests: glTextureGenDelete, TextureDestruction

--- Workshop/test/TextureTests.cpp
+++ Workshop/test/TextureTests.cpp
@@ -2,7 +2,19 @@
 
 #include <Workshop/Texture.hpp>
 
-TEST_F(WorkshopTest, TextureTest) {
+TEST_F(WorkshopTest, glTextureGenDelete) {
+  uint32_t id;
+  glGenTextures(1, &id);
+  glBindTexture(GL_TEXTURE_2D, id);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 128, 128, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+  EXPECT_TRUE(glIsTexture(id));
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  glDeleteTextures(1, &id);
+  EXPECT_FALSE(glIsTexture(id));
+}
+
+TEST_F(WorkshopTest, TextureIdDimensions) {
   ws::Texture::Specs specs {
     .width = 1024,
     .height = 768,
@@ -17,4 +29,16 @@ TEST_F(WorkshopTest, TextureTest) {
   glGetTextureLevelParameteriv(tex.getId(), mipLevel, GL_TEXTURE_HEIGHT, &h);
   ASSERT_EQ(w, 1024);
   ASSERT_EQ(h, 768);
+}
+
+TEST_F(WorkshopTest, TextureDestruction) {
+  uint32_t id = 0;
+  EXPECT_FALSE(glIsTexture(id));
+  {
+    ws::Texture tex1{};
+    id = tex1.getId();
+    ASSERT_NE(id, ws::INVALID);
+    EXPECT_TRUE(glIsTexture(id));
+  }
+  EXPECT_FALSE(glIsTexture(id));
 }
