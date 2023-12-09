f12bfccda569c41569553894166fe605b003f556
Ugur Guney <ugurguney@gmail.com>
2023-12-09 00:30:06 -0500

    Init Shadow Mapping example app

---
+++ example-apps/09-learnopengl-shadow-mapping/main.cpp
@@ -0,0 +1,37 @@
+#include <Workshop/Workshop.hpp>
+
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/vec3.hpp>
+
+// #include <implot.h>
+// #include <stb_image.h>
+// #include <tiny_obj_loader.h>
+// #include <vivid/vivid.h>
+
+#include <iostream>
+
+int main() {
+  ws::Workshop workshop{800, 600, "Workshop App"};
+
+  while (!workshop.shouldStop()) {
+    workshop.beginFrame();
+
+    workshop.imGuiDrawAppWindow();
+
+    ImGui::Begin("Main");
+    static glm::vec3 bgColor{1, 0, 0};
+    ImGui::ColorEdit3("BG Color", glm::value_ptr(bgColor));
+    ImGui::End();
+
+    glClearColor(bgColor.x, bgColor.y, bgColor.z, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    workshop.endFrame();
+  }
+
+  return 0;
+}
