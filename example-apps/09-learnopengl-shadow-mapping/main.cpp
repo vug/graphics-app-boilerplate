10ec47196271b08cdcac4c4aaaa96c15481c82c7
Ugur Guney <ugurguney@gmail.com>
2023-12-11 09:03:45 -0500

    Init AssetManager that owns assets, and a Renderable only has references to them.
    This is to prevent duplicate asset creation when multiple Renderable use the same asset

--- example-apps/09-learnopengl-shadow-mapping/main.cpp
+++ example-apps/09-learnopengl-shadow-mapping/main.cpp
@@ -1,3 +1,9 @@
+#include <Workshop/Assets.hpp>
+#include <Workshop/Camera.hpp>
+#include <Workshop/Model.hpp>
+#include <Workshop/Shader.hpp>
+#include <Workshop/Texture.hpp>
+#include <Workshop/Transform.hpp>
 #include <Workshop/Workshop.hpp>
 
 #include <glad/gl.h>
@@ -8,23 +14,77 @@
 #include <glm/vec3.hpp>
 
 import std.core;
+import std.filesystem;
+
+class AssetManager {
+ public:
+  std::unordered_map<std::string, ws::Mesh> meshes;
+  std::unordered_map<std::string, ws::Texture> textures;
+  std::unordered_map<std::string, ws::Shader> shaders;
+};
+
+struct Renderable {
+  std::string name;
+  ws::Transform transform;
+  ws::Mesh& mesh;
+  ws::Shader& shader;
+  ws::Texture& texture;
+};
+using Scene = std::vector<Renderable>;
 
 int main() {
   std::println("Hi!");
   ws::Workshop workshop{800, 600, "Shadow Maps"};
+  AssetManager assetManager;
+  assetManager.meshes.emplace("cube", ws::Mesh{ws::loadOBJ(ws::ASSETS_FOLDER / "models/cube.obj")});
+  assetManager.meshes.emplace("quad", ws::Mesh{ws::loadOBJ(ws::ASSETS_FOLDER / "models/quad.obj")});
+  assetManager.textures.emplace("wood", ws::Texture{ws::ASSETS_FOLDER / "images/LearnOpenGL/container.jpg"});
+  assetManager.shaders.emplace("unlit", ws::Shader{ws::ASSETS_FOLDER / "shaders/unlit.vert", ws::ASSETS_FOLDER / "shaders/unlit.frag"});
+
+  Renderable ground = {
+      .name = "ground",
+      .transform{glm::vec3{0, 0, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{5, 1, 5}},
+      .mesh{assetManager.meshes.at("quad")},
+      .shader = assetManager.shaders["unlit"],
+      .texture = assetManager.textures["wood"],
+  };
+  Scene scene{ground};
+
+  ws::PerspectiveCamera3D cam;
+  ws::AutoOrbitingCamera3DViewController orbitingCamController{cam};
+  orbitingCamController.radius = 7.f;
+  orbitingCamController.theta = 0.355f;
 
   while (!workshop.shouldStop()) {
     workshop.beginFrame();
+    const glm::uvec2 winSize = workshop.getWindowSize();
 
     workshop.imGuiDrawAppWindow();
 
     ImGui::Begin("Main");
-    static glm::vec3 bgColor{1, 0, 0};
+    static glm::vec3 bgColor{144.f/255, 225.f/255, 236.f/255};
     ImGui::ColorEdit3("BG Color", glm::value_ptr(bgColor));
     ImGui::End();
 
+    orbitingCamController.update(0.01f);
+    cam.aspectRatio = static_cast<float>(winSize.x) / winSize.y;
+
     glClearColor(bgColor.x, bgColor.y, bgColor.z, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    for (auto& renderable : scene) {
+      renderable.shader.bind();
+      renderable.mesh.bind();
+      glBindTextureUnit(0, renderable.texture.getId());
+      renderable.shader.setVector3("u_CameraPos", cam.getPosition());
+      renderable.shader.setMatrix4("u_ViewFromWorld", cam.getViewFromWorld());
+      renderable.shader.setMatrix4("u_ProjectionFromView", cam.getProjectionFromView());
+      renderable.shader.setMatrix4("u_WorldFromObject", renderable.transform.getWorldFromObjectMatrix());
+      renderable.mesh.draw();
+      glBindTextureUnit(0, 0);
+      renderable.mesh.unbind();
+      renderable.shader.unbind();
+    }
 
     workshop.endFrame();
   }
