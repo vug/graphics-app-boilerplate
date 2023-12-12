d04ded9ae7d9ea565411d0ee88ce2b10edbcd572
Ugur Guney <ugurguney@gmail.com>
2023-12-11 19:16:35 -0500

    LearnOpenGL scene cubes and ground Also resizeable window with depth on

--- example-apps/09-learnopengl-shadow-mapping/main.cpp
+++ example-apps/09-learnopengl-shadow-mapping/main.cpp
@@ -10,6 +10,7 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <imgui.h>
+#include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/vec3.hpp>
 
@@ -34,7 +35,7 @@ using Scene = std::vector<Renderable>;
 
 int main() {
   std::println("Hi!");
-  ws::Workshop workshop{800, 600, "Shadow Maps"};
+  ws::Workshop workshop{2048, 1536, "Shadow Maps"};
   AssetManager assetManager;
   assetManager.meshes.emplace("cube", ws::Mesh{ws::loadOBJ(ws::ASSETS_FOLDER / "models/cube.obj")});
   assetManager.meshes.emplace("quad", ws::Mesh{ws::loadOBJ(ws::ASSETS_FOLDER / "models/quad.obj")});
@@ -43,21 +44,45 @@ int main() {
 
   Renderable ground = {
       .name = "ground",
-      .transform{glm::vec3{0, 0, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{5, 1, 5}},
+      .transform{glm::vec3{0, -0.5, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{25.f, 1, 25.f}},
       .mesh{assetManager.meshes.at("quad")},
       .shader = assetManager.shaders["unlit"],
       .texture = assetManager.textures["wood"],
   };
-  Scene scene{ground};
+  Renderable cube1 = {
+      .name = "cube1",
+      .transform{glm::vec3{0, 1.5f, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{1.f, 1.f, 1.f}},
+      .mesh{assetManager.meshes.at("cube")},
+      .shader = assetManager.shaders["unlit"],
+      .texture = assetManager.textures["wood"],
+  };
+  Renderable cube2 = {
+      .name = "cube2",
+      .transform{glm::vec3{2.0f, 0.0f, 1.0f}, glm::vec3{0, 0, 1}, 0, glm::vec3{1.f, 1.f, 1.f}},
+      .mesh{assetManager.meshes.at("cube")},
+      .shader = assetManager.shaders["unlit"],
+      .texture = assetManager.textures["wood"],
+  };
+  Renderable cube3 = {
+      .name = "cube3",
+      .transform{glm::vec3{-1.f, 0, 2.f}, glm::normalize(glm::vec3{1.f, 0, 1.f}), glm::radians(60.f), glm::vec3{.5f, .5f, .5f}},
+      .mesh{assetManager.meshes.at("cube")},
+      .shader = assetManager.shaders["unlit"],
+      .texture = assetManager.textures["wood"],
+  };
+  Scene scene{ground, cube1, cube2, cube3,};
 
   ws::PerspectiveCamera3D cam;
   ws::AutoOrbitingCamera3DViewController orbitingCamController{cam};
-  orbitingCamController.radius = 7.f;
-  orbitingCamController.theta = 0.355f;
+  orbitingCamController.radius = 7.7f;
+  orbitingCamController.theta = 0.5;
+
+  glEnable(GL_DEPTH_TEST);
 
   while (!workshop.shouldStop()) {
     workshop.beginFrame();
     const glm::uvec2 winSize = workshop.getWindowSize();
+    glViewport(0, 0, winSize.x, winSize.y);
 
     workshop.imGuiDrawAppWindow();
 
@@ -70,7 +95,7 @@ int main() {
     cam.aspectRatio = static_cast<float>(winSize.x) / winSize.y;
 
     glClearColor(bgColor.x, bgColor.y, bgColor.z, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     for (auto& renderable : scene) {
       renderable.shader.bind();
