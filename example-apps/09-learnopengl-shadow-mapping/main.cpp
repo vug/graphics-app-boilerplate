ace7cb2c2e6e04829b6f9b6b3ae04134a23498f7
Ugur Guney <ugurguney@gmail.com>
2023-12-14 07:26:56 -0500

    Introduce Object base class, and Renderable and Camera Object derived classes.
    Scene's renderables and cameras members have vectors of them.
    Construct a vector of all Object, iterate over them and act based on type

--- example-apps/09-learnopengl-shadow-mapping/main.cpp
+++ example-apps/09-learnopengl-shadow-mapping/main.cpp
@@ -24,58 +24,102 @@ class AssetManager {
   std::unordered_map<std::string, ws::Shader> shaders;
 };
 
-struct Renderable {
+struct Object {
   std::string name;
   ws::Transform transform;
+};
+
+struct RenderableObject : public Object {
   ws::Mesh& mesh;
   ws::Shader& shader;
   ws::Texture& texture;
 };
-using Scene = std::vector<Renderable>;
+
+struct CameraObject : public Object {
+  ws::PerspectiveCamera3D camera;
+};
+
+class Scene {
+ public:
+  std::vector<RenderableObject> renderables;
+  std::vector<CameraObject> cameras;
+};
 
 int main() {
   std::println("Hi!");
   ws::Workshop workshop{2048, 1536, "Shadow Maps"};
+
   AssetManager assetManager;
   assetManager.meshes.emplace("cube", ws::Mesh{ws::loadOBJ(ws::ASSETS_FOLDER / "models/cube.obj")});
   assetManager.meshes.emplace("quad", ws::Mesh{ws::loadOBJ(ws::ASSETS_FOLDER / "models/quad.obj")});
   assetManager.textures.emplace("wood", ws::Texture{ws::ASSETS_FOLDER / "images/LearnOpenGL/container.jpg"});
   assetManager.shaders.emplace("unlit", ws::Shader{ws::ASSETS_FOLDER / "shaders/unlit.vert", ws::ASSETS_FOLDER / "shaders/unlit.frag"});
 
-  Renderable ground = {
-      .name = "ground",
-      .transform{glm::vec3{0, -0.5, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{25.f, 1, 25.f}},
-      .mesh{assetManager.meshes.at("quad")},
-      .shader = assetManager.shaders["unlit"],
-      .texture = assetManager.textures["wood"],
+  RenderableObject ground = {
+    Object{std::string{"Ground"}, ws::Transform{glm::vec3{0, -0.5, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{25.f, 1, 25.f}}},
+    assetManager.meshes.at("quad"),
+    assetManager.shaders["unlit"],
+    assetManager.textures["wood"],
   };
-  Renderable cube1 = {
-      .name = "cube1",
-      .transform{glm::vec3{0, 1.5f, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{1.f, 1.f, 1.f}},
-      .mesh{assetManager.meshes.at("cube")},
-      .shader = assetManager.shaders["unlit"],
-      .texture = assetManager.textures["wood"],
+  RenderableObject cube1 = {
+    {"Cube1", {glm::vec3{0, 1.5f, 0}, glm::vec3{0, 0, 1}, 0, glm::vec3{1.f, 1.f, 1.f}}},
+    assetManager.meshes.at("cube"),
+    assetManager.shaders["unlit"],
+    assetManager.textures["wood"],
   };
-  Renderable cube2 = {
-      .name = "cube2",
-      .transform{glm::vec3{2.0f, 0.0f, 1.0f}, glm::vec3{0, 0, 1}, 0, glm::vec3{1.f, 1.f, 1.f}},
-      .mesh{assetManager.meshes.at("cube")},
-      .shader = assetManager.shaders["unlit"],
-      .texture = assetManager.textures["wood"],
+  RenderableObject cube2 = {
+    Object{std::string{"Cube2"}, ws::Transform{glm::vec3{2.0f, 0.0f, 1.0f}, glm::vec3{0, 0, 1}, 0, glm::vec3{1.f, 1.f, 1.f}}},
+    assetManager.meshes.at("cube"),
+    assetManager.shaders["unlit"],
+    assetManager.textures["wood"],
   };
-  Renderable cube3 = {
-      .name = "cube3",
-      .transform{glm::vec3{-1.f, 0, 2.f}, glm::normalize(glm::vec3{1.f, 0, 1.f}), glm::radians(60.f), glm::vec3{.5f, .5f, .5f}},
-      .mesh{assetManager.meshes.at("cube")},
-      .shader = assetManager.shaders["unlit"],
-      .texture = assetManager.textures["wood"],
+  RenderableObject cube3 = {
+    Object{std::string{"Cube3"}, ws::Transform{glm::vec3{-1.f, 0, 2.f}, glm::normalize(glm::vec3{1.f, 0, 1.f}), glm::radians(60.f), glm::vec3{.5f, .5f, .5f}}},
+    assetManager.meshes.at("cube"),
+    assetManager.shaders["unlit"],
+    assetManager.textures["wood"],
   };
-  Scene scene{ground, cube1, cube2, cube3,};
-
-  ws::PerspectiveCamera3D cam;
+  CameraObject cam1{
+    Object{std::string{"SceneCamera"}, ws::Transform{glm::vec3{0, 0, -5.f}, glm::vec3{0, 0, 1}, 0, glm::vec3{1, 1, 1}}},
+  };
+  Scene scene{
+    .renderables{ground, cube1, cube2, cube3},
+    .cameras{cam1}
+  };
+  cube1.parent = &ground;
+  ws::PerspectiveCamera3D& cam = scene.cameras[0].camera;
   ws::AutoOrbitingCamera3DViewController orbitingCamController{cam};
   orbitingCamController.radius = 7.7f;
   orbitingCamController.theta = 0.5;
+
+  //std::vector<std::reference_wrapper<Object>> objects = {scene.renderables[0], scene.renderables[1], scene.renderables[2], scene.renderables[3], scene.cameras[0]};
+  //for (const auto& obj : objects) {
+  //  std::println("object name {}", obj.get().name);
+  //}
+  // 
+  //RenderableObject& r = reinterpret_cast<RenderableObject&>(objects[0].get());
+  //std::println("r name {} verts {}", r.name, r.mesh.meshData.vertices.size());
+
+  using RenderableObjectRef = std::reference_wrapper<RenderableObject>;
+  using CameraObjectRef = std::reference_wrapper<CameraObject>;
+  using VObject = std::variant<RenderableObjectRef, CameraObjectRef>;
+  std::vector<VObject> objects;
+  std::ranges::copy(scene.renderables, std::back_inserter(objects));
+  std::ranges::copy(scene.cameras, std::back_inserter(objects));
+
+  for (const auto& obj : objects) {
+    if (std::holds_alternative<RenderableObjectRef>(obj)) {
+      RenderableObject& ref = std::get<RenderableObjectRef>(obj).get();
+      std::println("RenderableObject name {} verts {} tr.pos.x {}", ref.name, ref.mesh.meshData.vertices.size(), ref.transform.position.x);
+    }
+    else if (std::holds_alternative<CameraObjectRef>(obj)) {
+      CameraObject& ref = std::get<CameraObjectRef>(obj).get();
+      std::println("CameraObject name {} verts fov {} tr.pos.x {}", ref.name, ref.camera.fov, ref.transform.position.x);
+    }
+    else
+      throw "unknown object type";
+  }
+
 
   glEnable(GL_DEPTH_TEST);
 
@@ -97,7 +141,7 @@ int main() {
     glClearColor(bgColor.x, bgColor.y, bgColor.z, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    for (auto& renderable : scene) {
+    for (auto& renderable : scene.renderables) {
       renderable.shader.bind();
       renderable.mesh.bind();
       glBindTextureUnit(0, renderable.texture.getId());
@@ -113,6 +157,5 @@ int main() {
 
     workshop.endFrame();
   }
-
   return 0;
 }
