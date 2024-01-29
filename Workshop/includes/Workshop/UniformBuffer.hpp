c1632fe4123117931bf3f40b61232e09f28541ed
Ugur Guney <ugurguney@gmail.com>
2024-01-29 00:06:47 -0500

    new templated UniformBuffer class that allocates memory for template parameter struct size, and has a public member of that type, and has upload method to upload that struct into buffer

---
+++ Workshop/includes/Workshop/UniformBuffer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Common.hpp"
+
+#include <glad/gl.h>
+
+#include <format>
+#include <string>
+
+namespace ws {
+template <typename TUniformStruct>
+class UniformBuffer {
+ public:
+  UniformBuffer(uint32_t uniformBlockBindingPoint)
+      : id([]() { uint32_t uboId; glCreateBuffers(1, &uboId); return uboId; }()),
+        debugName(std::format("UniformBuffer[{}]", static_cast<uint32_t>(id))) {
+    glNamedBufferStorage(id, sizeof(TUniformStruct), nullptr, GL_DYNAMIC_STORAGE_BIT);
+    glBindBufferBase(GL_UNIFORM_BUFFER, uniformBlockBindingPoint, id);
+  }
+  UniformBuffer(const UniformBuffer& other) = delete;
+  UniformBuffer& operator=(const UniformBuffer& other) = delete;
+  UniformBuffer(UniformBuffer&& other) = default;
+  UniformBuffer& operator=(UniformBuffer&& other) = default;
+  ~UniformBuffer() {
+    glDeleteBuffers(1, &id);
+  }
+
+  void upload() {
+    glNamedBufferSubData(id, 0, sizeof(TUniformStruct), &uniforms);
+  }
+
+  TUniformStruct uniforms{};
+
+private:
+  GlHandle id = INVALID;
+  std::string debugName;
+};
+}
