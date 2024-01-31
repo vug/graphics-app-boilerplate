fee9f18bb2fc0784dd0fe81ded3e4ed594bff144
Ugur Guney <ugurguney@gmail.com>
2024-01-30 22:25:53 -0500

    bring back to `uploadToShader` methods of light structs for non-UBO usages.

--- Workshop/Lights.cpp
+++ Workshop/Lights.cpp
@@ -1,5 +1,30 @@
 #include "Lights.hpp"
 
+#include <format>
+
 namespace ws {
+
+void AmbientLight::uploadToShader(const Shader& shader, const std::string& name) const {
+  shader.setVector3(std::format("{}.color", name).c_str(), color);
+}
+
+void HemisphericalLight::uploadToShader(const Shader& shader, const std::string& name) const {
+  shader.setVector3(std::format("{}.northColor", name).c_str(), northColor);
+  shader.setFloat(std::format("{}.intensity", name).c_str(), intensity);
+  shader.setVector3(std::format("{}.southColor", name).c_str(), southColor);
+}
+
+void PointLight::uploadToShader(const Shader& shader, const std::string& name) const {
+  shader.setVector3(std::format("{}.position", name).c_str(), position);
+  shader.setFloat(std::format("{}.intensity", name).c_str(), intensity);
+  shader.setVector3(std::format("{}.color", name).c_str(), color);
+}
+
+void DirectionalLight::uploadToShader(const Shader& shader, const std::string& name) const {
+  shader.setVector3(std::format("{}.position", name).c_str(), position);
+  shader.setFloat(std::format("{}.intensity", name).c_str(), intensity);
+  shader.setVector3(std::format("{}.direction", name).c_str(), direction);
+  shader.setVector3(std::format("{}.color", name).c_str(), color);
+}
 
 } // namespace ws
--- Workshop/includes/Workshop/Lights.hpp
+++ Workshop/includes/Workshop/Lights.hpp
@@ -1,23 +1,31 @@
 #pragma once
 
+#include "Shader.hpp"
+
 #include <glm/vec3.hpp>
 
 namespace ws {
 
 struct AmbientLight {
   glm::vec3 color;
+
+  void uploadToShader(const Shader& shader, const std::string& name) const;
 };
 
 struct HemisphericalLight {
   glm::vec3 northColor;
   float intensity;
   glm::vec3 southColor;
+
+  void uploadToShader(const Shader& shader, const std::string& name) const;
 };
 
 struct PointLight {
   glm::vec3 position;
   float intensity;
   glm::vec3 color;
+
+  void uploadToShader(const Shader& shader, const std::string& name) const;
 };
 
 struct DirectionalLight {
@@ -25,5 +33,7 @@ struct DirectionalLight {
   float intensity;
   glm::vec3 direction;
   glm::vec3 color;
+
+  void uploadToShader(const Shader& shader, const std::string& name) const;
 };
 }
