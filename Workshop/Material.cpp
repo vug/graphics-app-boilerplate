2f2b7d02b562b0b341ca5330356fa1141731967c
Ugur Guney <ugurguney@gmail.com>
2024-02-01 20:03:54 -0500

    use unordered_map to simplify search logic in doParametersAndUniformsMatch

--- Workshop/Material.cpp
+++ Workshop/Material.cpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 namespace rng = std::ranges;
 namespace vws = rng::views;
@@ -24,21 +25,21 @@ Material::Material(Shader& shader)
 
 bool Material::doParametersAndUniformsMatch() const {
   std::vector<UniformInfo> uniformInfos = shader.getUniformInfos();
-  std::vector<std::string> uniformNames = uniformInfos | vws::transform(&UniformInfo::name) | rng::to<std::vector>();
-  std::vector<std::string> parameterNames;
-  for (const auto& [pName, pVal] : parameters)
-    parameterNames.emplace_back(pName);
+  std::unordered_set<std::string> uniformNames = uniformInfos | vws::transform(&UniformInfo::name) | rng::to<std::unordered_set>();
+  std::unordered_set<std::string> parameterNames = parameters | vws::keys | rng::to<std::unordered_set>();
 
   // Uniforms (that are not per mesh and not in a uniform block) without corresponding Material parameters. A material should provide a parameter for every uniform that needs it.
   auto uniformsWithoutParameters = uniformInfos 
-    | vws::filter([&](auto& ui) { return rng::find(parameterNames, ui.name) == parameterNames.end() && ui.offset == -1 && !Material::perMeshUniforms.contains(ui.name); }) 
+    | vws::filter([&](auto& ui) { return !parameterNames.contains(ui.name) && ui.offset == -1 && !Material::perMeshUniforms.contains(ui.name); }) 
     | rng::to<std::vector>();
   for (const auto& ui : uniformsWithoutParameters)
     std::println("uniform {} has no corresponding parameter", ui.name);
+
   // Material parameters without corresponding uniforms in the Shader. Materials shouldn't have unnecessary parameters.
-  auto parametersWithoutUniforms = parameterNames | vws::filter([&](auto& pName) { return rng::find(uniformNames, pName) == uniformNames.end(); }) | rng::to<std::vector>();
+  auto parametersWithoutUniforms = parameterNames | vws::filter([&](auto& pName) { return !uniformNames.contains(pName); }) | rng::to<std::vector>();
   for (const auto& pName : parametersWithoutUniforms)
     std::println("parameter {} has no corresponding uniform", pName);
+
   return !uniformsWithoutParameters.empty() || !parametersWithoutUniforms.empty();
 }
 
