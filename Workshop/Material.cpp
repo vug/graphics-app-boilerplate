e95c90952d000cd9063c7830e23f486613744fa7
Ugur Guney <ugurguney@gmail.com>
2024-02-01 21:24:16 -0500

    Also compare the types of material parameters and corresponding uniforms.

--- Workshop/Material.cpp
+++ Workshop/Material.cpp
@@ -29,18 +29,34 @@ bool Material::doParametersAndUniformsMatch() const {
   std::unordered_set<std::string> parameterNames = parameters | vws::keys | rng::to<std::unordered_set>();
 
   // Uniforms (that are not per mesh and not in a uniform block) without corresponding Material parameters. A material should provide a parameter for every uniform that needs it.
-  auto uniformsWithoutParameters = uniformInfos 
+  const auto uniformsWithoutParameters = uniformInfos 
     | vws::filter([&](auto& ui) { return !parameterNames.contains(ui.name) && ui.offset == -1 && !Material::perMeshUniforms.contains(ui.name); }) 
     | rng::to<std::vector>();
   for (const auto& ui : uniformsWithoutParameters)
-    std::println("uniform {} has no corresponding parameter", ui.name);
+    std::println("Uniform {} has no corresponding parameter.", ui.name);
 
   // Material parameters without corresponding uniforms in the Shader. Materials shouldn't have unnecessary parameters.
-  auto parametersWithoutUniforms = parameterNames | vws::filter([&](auto& pName) { return !uniformNames.contains(pName); }) | rng::to<std::vector>();
+  const auto parametersWithoutUniforms = parameterNames | vws::filter([&](auto& pName) { return !uniformNames.contains(pName); }) | rng::to<std::vector>();
   for (const auto& pName : parametersWithoutUniforms)
-    std::println("parameter {} has no corresponding uniform", pName);
+    std::println("Parameter {} has no corresponding uniform.", pName);
 
-  return !uniformsWithoutParameters.empty() || !parametersWithoutUniforms.empty();
+  auto uniformsWithParameters = uniformInfos | vws::filter([&](auto& ui) { return parameterNames.contains(ui.name); });
+  bool typesMatch = true;
+  for (const UniformInfo& ui : uniformsWithParameters) {
+    const ParamT& param = parameters.at(ui.name);
+    const bool match = std::visit(Overloaded{
+      [&]([[maybe_unused]] int _) { return ui.glType == GL_INT; },
+      [&]([[maybe_unused]] float _) { return ui.glType == GL_FLOAT; },
+      [&]([[maybe_unused]] const glm::vec2& _) { return ui.glType == GL_FLOAT_VEC2; },
+      [&]([[maybe_unused]] const glm::vec3& _) { return ui.glType == GL_FLOAT_VEC3; },
+    }, param);
+    if (!match)
+      std::println("Material parameter '{}' is of type '{}' which is different than the type of the corresponding shader uniform, '{}'.", 
+        ui.name, std::visit([](auto&& p) { return typeid(p).name(); }, param), Shader::UNIFORM_AND_ATTRIBUTE_TYPES[ui.glType]);
+    typesMatch &= match;
+  }
+
+  return !uniformsWithoutParameters.empty() || !parametersWithoutUniforms.empty() || !typesMatch;
 }
 
 void Material::uploadUniform(const std::string& name, const ParamT& value) const {
