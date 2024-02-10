614899be3097691680147caca233a43186f07061
Ugur Guney <ugurguney@gmail.com>
2024-02-10 12:55:13 -0500

    Migrate Cubemap to DSA

--- Workshop/Cubemap.cpp
+++ Workshop/Cubemap.cpp
@@ -10,13 +10,14 @@ void Cubemap::loadFace(GLenum faceLabel, const path& path) {
   unsigned char* data = stbi_load(path.string().c_str(), &width, &height, &nrChannels, 0);
   assert(nrChannels == 3);
   assert(data);
-  glTexImage2D(faceLabel, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+  if (faceLabel == GL_TEXTURE_CUBE_MAP_POSITIVE_X)
+    glTextureStorage2D(id, 1, GL_RGB8, width, height);
+  glTextureSubImage3D(id, 0, 0, 0, faceLabel - GL_TEXTURE_CUBE_MAP_POSITIVE_X, width, height, 1, GL_RGB, GL_UNSIGNED_BYTE, data);
   stbi_image_free(data);
 }
 
 Cubemap::Cubemap(const path& right, const path& left, const path& top, const path& bottom, const path& front, const path& back) {
-  glGenTextures(1, &id);
-  bind();
+  glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &id);
 
   loadFace(GL_TEXTURE_CUBE_MAP_POSITIVE_X, right);
   loadFace(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, left);
@@ -25,13 +26,11 @@ Cubemap::Cubemap(const path& right, const path& left, const path& top, const pat
   loadFace(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, front);
   loadFace(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, back);
 
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-  unbind();
+  glTextureParameteri(id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTextureParameteri(id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTextureParameteri(id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTextureParameteri(id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTextureParameteri(id, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 }
 
 Cubemap::~Cubemap() {
