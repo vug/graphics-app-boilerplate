22365d622657415d60c96bc3b289524d3e2ba097
Ugur Guney <ugurguney@gmail.com>
2024-02-28 21:19:23 -0500

    replacing Vec's with glm::vec3's (4)

--- example-apps/15-smallpaint/smallpaint_painterly.cpp
+++ example-apps/15-smallpaint/smallpaint_painterly.cpp
@@ -202,30 +202,30 @@ int main() {
 	srand(time(NULL));
 	pl params;
 	vector<Obj*> scene;
-	auto add=[&scene](Obj* s, Vec cl, double emission, int type) {
-			s->setMat(cl.v,emission,type);
+	auto add=[&scene](Obj* s, glm::dvec3 cl, double emission, int type) {
+			s->setMat(cl,emission,type);
 			scene.push_back(s);
 	};
 
 	// Radius, position, color, emission, type (1=diff, 2=spec, 3=refr) for spheres
-	add(new Sphere(1.05,glm::dvec3(1.45,-0.75,-4.4)),Vec(4,8,4),0,2); // Middle sphere
-	add(new Sphere(0.45,glm::dvec3(2.05,0.8,-3.7)),Vec(10,10,1),0,3); // Right sphere
-	add(new Sphere(0.6,glm::dvec3(1.95,-1.75,-3.1)),Vec(4,4,12),0,1); // Left sphere
+	add(new Sphere(1.05,glm::dvec3(1.45,-0.75,-4.4)),glm::dvec3(4,8,4),0,2); // Middle sphere
+	add(new Sphere(0.45,glm::dvec3(2.05,0.8,-3.7)),glm::dvec3(10,10,1),0,3); // Right sphere
+	add(new Sphere(0.6,glm::dvec3(1.95,-1.75,-3.1)),glm::dvec3(4,4,12),0,1); // Left sphere
 	// Position, normal, color, emission, type for planes
-	add(new Plane(2.5,glm::dvec3(-1,0,0)),Vec(6,6,6),0,1); // Bottom plane
-	add(new Plane(5.5,glm::dvec3(0,0,1)),Vec(6,6,6),0,1); // Back plane
-	add(new Plane(2.75,glm::dvec3(0,1,0)),Vec(10,2,2),0,1); // Left plane
-	add(new Plane(2.75,glm::dvec3(0,-1,0)),Vec(2,10,2),0,1); // Right plane
-	add(new Plane(3.0,glm::dvec3(1,0,0)),Vec(6,6,6),0,1); // Ceiling plane
-	add(new Plane(0.5,glm::dvec3(0,0,-1)),Vec(6,6,6),0,1); // Front plane
+	add(new Plane(2.5,glm::dvec3(-1,0,0)),glm::dvec3(6,6,6),0,1); // Bottom plane
+	add(new Plane(5.5,glm::dvec3(0,0,1)),glm::dvec3(6,6,6),0,1); // Back plane
+	add(new Plane(2.75,glm::dvec3(0,1,0)),glm::dvec3(10,2,2),0,1); // Left plane
+	add(new Plane(2.75,glm::dvec3(0,-1,0)),glm::dvec3(2,10,2),0,1); // Right plane
+	add(new Plane(3.0,glm::dvec3(1,0,0)),glm::dvec3(6,6,6),0,1); // Ceiling plane
+	add(new Plane(0.5,glm::dvec3(0,0,-1)),glm::dvec3(6,6,6),0,1); // Front plane
 	const glm::dvec3 L0 = (glm::dvec3(-1.9,0,-3));
-	add(new Sphere(eps,L0),Vec(0,0,0),120,1); // Light
+	add(new Sphere(eps,L0),glm::dvec3(0,0,0),120,1); // Light
 	params["refr_index"] = 1.9;
 	params["spp"] = 8.0; // samples per pixel
 
-	Vec **pix = new Vec*[width];
+	glm::dvec3 **pix = new glm::dvec3*[width];
 	for(int i=0;i<width;i++) {
-			pix[i] = new Vec[height];
+			pix[i] = new glm::dvec3[height];
 	}
 
 	const double spp = params["spp"];
@@ -244,14 +244,14 @@ int main() {
 				Vec c;
 				Ray ray;
 				ray.o = {};
-				Vec cam = camcr(i,j);
-				cam.v.x = cam.v.x + RND/700;
-				cam.v.y = cam.v.y + RND/700;
-				ray.d = glm::normalize(cam.v - ray.o);
+				glm::dvec3 cam = camcr(i,j);
+				cam.x = cam.x + RND/700;
+				cam.y = cam.y + RND/700;
+				ray.d = glm::normalize(cam - ray.o);
 				trace(ray,scene,0,c,params,hal,hal2);
-				pix[i][j].v.x += c.v.x*(1/spp);
-				pix[i][j].v.y += c.v.y*(1/spp);
-				pix[i][j].v.z += c.v.z*(1/spp);
+				pix[i][j].x += c.v.x*(1/spp);
+				pix[i][j].y += c.v.y*(1/spp);
+				pix[i][j].z += c.v.z*(1/spp);
 			}
 		}
 	}
@@ -260,9 +260,9 @@ int main() {
 	for (size_t j = 0; j < height; j++) {
 		for (size_t i = 0; i < width; i++) {
 			const size_t ix = (j * width + i) * 3;
-			pixels[ix + 0] = std::min((int)pix[j][i].v.x, 255);
-			pixels[ix + 1] = std::min((int)pix[j][i].v.y, 255);
-			pixels[ix + 2] = std::min((int)pix[j][i].v.z, 255);
+			pixels[ix + 0] = std::min((int)pix[j][i].x, 255);
+			pixels[ix + 1] = std::min((int)pix[j][i].y, 255);
+			pixels[ix + 2] = std::min((int)pix[j][i].z, 255);
 		}
 	}
 	stbi_write_png("ray.png", width, height, 3, pixels.data(), sizeof(uint8_t) * width * 3);
