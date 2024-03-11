ca896699352671be5127426faa08de358df1e78f
Ugur Guney <ugurguney@gmail.com>
2024-03-11 00:23:26 -0400

    various tonemapping types

--- example-apps/16-embree/EmbreeUtils.hpp
+++ example-apps/16-embree/EmbreeUtils.hpp
@@ -78,4 +78,57 @@ class Image {
   int numChannels;
   std::vector<glm::vec3> pixels;
 };
+
+class ToneMapper {
+ public:
+  static glm::vec3 toGamma(const glm::vec3& rgb) {
+    return glm::pow(abs(rgb), glm::vec3(1.0f / 2.2f));
+  }
+  static glm::vec3 toReinhard1(const glm::vec3& rgb) {
+    float lumRgb = lumf(rgb);
+    float lumScale = lumRgb / (lumRgb + 1.0f);
+    return toGamma(rgb * lumScale / lumRgb);
+  }
+  static glm::vec3 toReinhard2(const glm::vec3& rgb, float whiteSq) {
+    float lumRgb = lumf(rgb);
+    float lumScale = (lumRgb * (1.0f + lumRgb / whiteSq)) / (1.0f + lumRgb);
+    return toGamma(rgb * lumScale / lumRgb);
+  }
+  static glm::vec3 toFilmic(const glm::vec3& rgb) {
+    glm::vec3 res = glm::max(glm::vec3(0.0f), rgb - 0.004f);
+    res = (res * (6.2f * res + 0.5f)) / (res * (6.2f * res + 1.7f) + 0.06f);
+    return res;
+  }
+  static glm::vec3 toUncharted2b(const glm::vec3& rgb) {
+    float exposureBias = 2.0f;
+    glm::vec3 curr = exposureBias * toUncharted2(rgb);
+
+    float w = 11.2f;
+    glm::vec3 whiteScale = 1.0f / toUncharted2(w);
+
+    const glm::vec3 rgb2 = curr * whiteScale;
+    return toGamma(rgb2);
+  }
+
+private:
+  static glm::vec3 toUncharted2(const glm::vec3& v) {
+    float a = 0.22f;
+    float b = 0.30f;
+    float c = 0.10f;
+    float d = 0.20f;
+    float e = 0.01f;
+    float f = 0.30f;
+    //float w = 11.2f;
+    return ((v * (a * v + c * b) + d * e) / (v * (a * v + b) + d * f)) - e / f;
+  }
+
+  static glm::vec3 toUncharted2(float x) {
+    glm::vec3 v = glm::vec3(x);
+    return toUncharted2(v);
+  }
+
+  static float lumf(glm::vec3 rgb) {
+    return glm::dot(glm::vec3(0.2126729, 0.7151522, 0.0721750), rgb);
+  }
+};
 }  // namespace ws
